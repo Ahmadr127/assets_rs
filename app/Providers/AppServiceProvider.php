a59a7ce58371d067baadfab0c667f4a218d93d8c
ahmadr127 <ahmdrifai808@gmail.com>
2025-12-31 14:20:12 +0700

    update https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,11 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register custom middleware
         $this->app['router']->aliasMiddleware('permission', \App\Http\Middleware\CheckPermission::class);
+        
+        // Force HTTPS in production (required for Cloudflare/reverse proxy)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
+
